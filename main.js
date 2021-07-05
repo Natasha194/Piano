22152f5954f85cc90b34dba8fdfe0e02be6618d4
Natasha194 <77156649+Natasha194@users.noreply.github.com>
2021-07-05 18:35:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+function lowerA() {
+    document.getElementById("lowerAaudio").play();
+}
+
+function lowerB() {
+    document.getElementById("lowerBaudio").play();
+}
+
+function lowerBb() {
+    document.getElementById("lowerBbaudio").play();
+}
+
+function middleC() {
+    document.getElementById("middleCaudio").play();
+}
+
+function middleDb() {
+    document.getElementById("middleDbaudio").play();
+}
+
+function middleD() {
+    document.getElementById("middleDaudio").play();
+}
+
+function middleE() {
+    document.getElementById("middleEaudio").play();
+}
+
+function middleEb() {
+    document.getElementById("middleEbaudio").play();
+}
+
+function middleF() {
+    document.getElementById("middleEbaudio").play();
+}
+
+function middleG() {
+    document.getElementById("middleGaudio").play();
+}
+function middleGb() {
+    document.getElementById("middleGbaudio").play();
+}
